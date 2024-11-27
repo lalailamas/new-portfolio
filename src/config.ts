aa021803f787609b82fb82683a52ba1f8f32406f
lalailamas <ale.cordoba.p@gmail.com>
2024-11-26 22:16:46 -0300

    initializing project

---
+++ src/config.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = 'Lalailamas | Personal Portfolio';
+export const SITE_DESCRIPTION = 'Personal Portfolio Website built with Astro and TailwindCSS';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
